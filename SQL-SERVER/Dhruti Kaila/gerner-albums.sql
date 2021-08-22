39c6c4fcf1b81c2c2c9e17023e61df01f572616b
niraj <niraj.surati01@gmail.com>
2021-08-22 16:35:00 +0530

    REFS: - and fixes: - Changed file type

--- SQL-SERVER/Dhruti Kaila/gerner-albums.sql
+++ SQL-SERVER/Dhruti Kaila/gerner-albums.sql
@@ -1,28 +1,33 @@
-INSERT INTO Albums (AlbumID ,ArtistId,Name ,Picture ,CountryID ,PublishedON ,Premium ,Active)
-
-VALUES(1,'1','Luke','Chocolat.png',2000-11-2,1,1);
- VALUES(2,'2','Liam','Iron Man.png ',2000-11-2,1,0);
- VALUES(3,'3','Angela','Brimstone.png',2000-11-2,1,0);
- VALUES(4,'4','Thea','Pretty Woman.png',2000-11-2,1,1);
- VALUES(5,'5','Felicity','Snatch.png',2000-11-2,1,1);
- VALUES(6,'6','Phillip','Iron Man.png ',2000-11-2,0,1);
- VALUES(7,'7','Celina','Seven.png',2000-11-2,0,1);
- VALUES(8,'8','Carolyn','The Godfather.png',2000-11-2,0,0);
- VALUES(9,'9','Russel','The Dark Tower.png',2000-11-2,0,0);
- VALUES(10,'10','Kurt','Alien.png', 2000-11-2,0,0);
-
+INSERT INTO Albums (ArtistId,Name ,Picture ,CountryID ,PublishedON )
+VALUES(1,'Luke','Chocolat.png',1,'2000-11-2')
+	,	(2,'Liam','Iron Man.png ',2,'2000-11-2')
+	,	(3,'Angela','Brimstone.png',5,'2000-11-2')
+	,	(4,'Thea','Pretty Woman.png',4,'2000-11-2')
+	,	(5,'Felicity','Snatch.png',1,'2000-11-2')
+	,	(5,'Phillip','Iron Man.png ',6,'2000-11-2')
+	,	(4,'Celina','Seven.png',7,'2000-11-2')
+	,	(3,'Carolyn','The Godfather.png',5,'2000-11-2')
+	,	(2,'Russel','The Dark Tower.png',2,'2000-11-2')
+	,	(1,'Kurt','Alien.png', 1,'2000-11-2')
 
 
 
-INSERT INTO Gernre (GenreId ,Genre );
 
-VALUES(02,'Dubstep');
-VALUES(69,'Drum & Bass');
- VALUES(92,'Jazz');
- VALUES(87,'Dubstep');
-  VALUES(83,'Reggae');
-  VALUES(63,'Baroque');
-  VALUES(12,'Techno');
-  VALUES(19,'Drum & Bass');
-  VALUES(42,'Soul');
- VALUES(21,'Opera');
+INSERT INTO Genres
+VALUES('Dubstep')
+	,	('Drum & Bass')
+	,	('Jazz')
+	,	('Dubstep')
+	,	('Reggae')
+	,	('Baroque')
+	,	('Techno')
+	,	('Drum & Bass')
+	,	('Soul')
+	,	('Opera')
+	
+INSERT INTO AlbumLike
+VALUES (2,1)
+	,	(2,2)
+
+
+TRUNCATE TABLE AlbumLIke 
